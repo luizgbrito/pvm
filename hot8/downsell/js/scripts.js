c952d77bc83ecb333dfeaf2e07f4e2b50ba69189
OMNI DIGITAL <105747272+luizgbrito@users.noreply.github.com>
2024-03-20 17:53:21 -0300

    Add files via upload

---
+++ hot8/downsell/js/scripts.js
@@ -0,0 +1,31 @@
+/*
+$.get("https://ssl.geoplugin.net/json.gp?k=292af9fb00c1353d", function (resp) {
+    var data = JSON.parse(resp);
+    alert('chegou aqui')
+    $("#location").html(data.geoplugin_city);
+});*/
+
+$(document).ready(function() { 
+
+    /*if (jQuery(window).width() < 480){
+        $('.hero').parallax({
+            imageSrc: 'https://atracaosecreta.com/main/img/bg-top-mobile.png',
+            naturalWidth: 600,
+            naturalHeight: 400
+        });
+    }
+
+    if (jQuery(window).width() > 480){
+        $('.hero').parallax({
+            imageSrc: 'https://atracaosecreta.com/main/img/bg-top.png',
+            naturalWidth: 600,
+            naturalHeight: 400
+        });
+    }
+
+    setTimeout(function() { 
+        $('#box').show(); 
+ }, 120  * 1 * 1000); */
+});
+
+ 
